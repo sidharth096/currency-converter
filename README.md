f3700f77ef94f3223942710387ac410c738a07d6
sidharth <sidharthtp096@gmail.com>
2024-10-05 00:10:57 +0530

    Add readme file

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# currency-converter
-An npm package to convert currency
+# currency-converter-pro
+
+An NPM package for effortless currency conversion with real-time exchange rates.
+
+## Getting started
+
+## Installation
+
+This package can be installed using `npm`
+
+```
+npm i currency-converter-lt
+
+```
+
+## Usage
+
+Import `currency-converter-pro`.
+
+```javascript
+import { convertCurrency } from "currency-converter-pro";
+```
+
+After you can make function call like this:
+
+Base Currency: The currency you're converting from.
+To Currency: The currency you're converting to.
+Units: The amount you want to convert.
+
+```javascript
+const res = await convertCurrency("USD", "INR", 1);
+console.log("res", res);
+```
+
+Here
+USD : Base Currency
+INR : To Currency
+1 : Units
+
+## Issues
+
+If any issues are found, they can be reported [here](https://github.com/sidharth096/currency-converter/issues).
+
+## License
+
+This project is licensed under Apache
+
+
--- index.js
+++ index.js
@@ -2,13 +2,22 @@ import Freecurrencyapi from '@everapi/freecurrencyapi-js';
 
 const freecurrencyapi = new Freecurrencyapi('fca_live_Lw9dZ7Kh7EUZmupAIOksUqHNN1nr6U6heLzUKKjX');
 
-export async function convertCurrency(fromCurrency,toCurrency,units) {
-    const res = await freecurrencyapi.latest({
-        base_currency: fromCurrency,
-        currencies: toCurrency
-    })
-    
-    const multiplier = res.data[toCurrency]    
-    return multiplier*units
-    
+export async function convertCurrency(fromCurrency, toCurrency, units) {
+    try {
+        // Fetch the latest exchange rates
+        const res = await freecurrencyapi.latest({
+            base_currency: fromCurrency,
+            currencies: toCurrency
+        });
+
+        // Check if the response has data for the requested currency
+        if (res?.data && res.data[toCurrency]) {
+            const multiplier = res.data[toCurrency];
+            return multiplier * units;
+        } else {
+            throw new Error(JSON.stringify(res.errors));
+        }
+    } catch (error) {
+       return error
+    }
 }
